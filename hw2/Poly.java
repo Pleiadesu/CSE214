c04d32fbf8547795414bd382e1b21776d387b6e5
Jey <jeykang0@gmail.com>
2018-09-13 12:58:18 +0900

    testing
    testing whether Poly.add() works with a single outer for loop rather than two nested ones
    result: success

--- hw2/Poly.java
+++ hw2/Poly.java
@@ -1,4 +1,4 @@
- 
+//NOTE: All non-inline comments are failed/changed attempts or debug code.
 
 //import ring.Comp;
 //import ring.Field;
@@ -31,16 +31,19 @@ public class Poly<F extends Field> implements Ring, Modulo, Ordered {
     public Ring add(Ring a) {
         Poly that = (Poly)a;
         F[] thatCoef = (F[])that.getCoef();
-        F[] newCoef;
+        F[] newCoef, smallerCoef;
         //System.out.println("thiscoef length: "+this.coef.length);
         //System.out.println("thatcoef length: "+thatCoef.length);
-        if(this == this.addIdentity()){
+        if(this == this.addIdentity()){ //Check to see if one of the vars is addIdentity, and skip the for loop
             return that;
         }
         else if(that == this.addIdentity()){
             return this;
         }
-        if(this.coef.length >= thatCoef.length){
+        if(this.coef.length >= thatCoef.length){ //Assign longer and shorter coefs
+            newCoef = this.coef.clone();
+            smallerCoef = thatCoef.clone();
+            /*
             //System.out.println("thiscoef >= thatcoef");
             newCoef = this.coef.clone();
             for(int i = 0; i < thatCoef.length; i++){
@@ -49,8 +52,12 @@ public class Poly<F extends Field> implements Ring, Modulo, Ordered {
                 newCoef[i] = (F)this.coef[i].add(thatCoef[i]);
                 //System.out.println("newCoef after: "+newCoef[i]);
             }
+            */
         }
         else{
+            newCoef = thatCoef.clone();
+            smallerCoef = this.coef.clone();
+            /*
             //System.out.println("thiscoef < thatcoef");
             newCoef = thatCoef.clone();
             for(int i = 0; i < this.coef.length; i++){
@@ -59,13 +66,20 @@ public class Poly<F extends Field> implements Ring, Modulo, Ordered {
                 newCoef[i] = (F)this.coef[i].add(thatCoef[i]);
                 //System.out.println("newCoef after: "+newCoef[i]);
             }
+            */
         }       
+        for(int i = 0; i < smallerCoef.length; i++){ //For every element in the shorter coef, add it to the corresponding element in the longer coef
+                //System.out.println("newCoef before: "+newCoef[i]);
+                //System.out.println(this.coef[i]+" added to "+thatCoef[i]+" = "+(F)this.coef[i].add(thatCoef[i]));                           
+                newCoef[i] = (F)newCoef[i].add(smallerCoef[i]);
+                //System.out.println("newCoef after: "+newCoef[i]);
+            }
         return new Poly(newCoef);
     }
     public Poly del(Poly that) {
-        return (Poly)this.add(that.addInverse());
+        return (Poly)this.add(that.addInverse()); //convenient method to handle adding addInverse for debug
     }
-    public Ring mul(Ring a) {
+    public Ring mul(Ring a) { //Direct conversion of .mul() from PolyDbl
         Poly that = (Poly)a;
         F[] thatCoef = (F[])(that.getCoef());
         F[] newCoef = (F[])new Field[this.coef.length + thatCoef.length - 1];
@@ -88,7 +102,7 @@ public class Poly<F extends Field> implements Ring, Modulo, Ordered {
         }
         return new Poly(newCoef);
     }
-    public Poly[] longdiv(Poly that) {
+    public Poly[] longdiv(Poly that) { //I was originally going to use my own solution for this, but mine used a while loop that was more clunky than the given solution for the recitation
         //return value: longdiv(...)[0]: quotient,
         //              longdiv(...)[1]: remainder
         
@@ -120,13 +134,13 @@ public class Poly<F extends Field> implements Ring, Modulo, Ordered {
         
         return new Poly[] {new Poly(q), new Poly(r)};
     }
-    public Ring addIdentity() {
+    public Ring addIdentity() { //There will always be an element in Poly, but java throws a tantrum if there isn't at least one return statement outside a conditional
         if(this.coef.length > 0){
             return new Poly((F[])(new Field[]{(F)this.coef[0].addIdentity()}));
         }
         return new Poly((F[])(new Field[]{}));
     }
-    public Ring addInverse() {
+    public Ring addInverse() { //Unlike PolyDbl there is no convenient way to multiply every element by -1
         F[] tempCoef = this.coef.clone();
         for(int i = 0; i < tempCoef.length; i++){
             tempCoef[i] = (F)tempCoef[i].addInverse();
@@ -148,15 +162,15 @@ public class Poly<F extends Field> implements Ring, Modulo, Ordered {
         }
         else if(this.coef.length == aCoef.length){
             boolean temp = true;
-            for(int i = this.coef.length - 1; i >= 0; i--){
+            for(int i = this.coef.length - 1; i >= 0; i--){ //Check if every element is equal
                 if(!Comp.eq(this.coef[i], aCoef[i])){
                     temp = false;
                 }
             }
-            if(temp){
-                return temp;
+            if(temp){ //if every element is same return true
+                return true;
             }
-            for(int i = this.coef.length - 1; i >= 0; i--){
+            for(int i = this.coef.length - 1; i >= 0; i--){ //Check every coef to see which is bigger
                 ////System.out.println("Checking if "+this.coef[i]+" is bigger than "+aCoef[i]);
                 if(Comp.lt(this.coef[i], aCoef[i])){
                     ////System.out.println(this+" is smaller than "+tempA);
