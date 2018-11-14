ff41c0e8ac8eb2c6fd2b6012bb5ba4255dea9672
Jey <jeykang0@gmail.com>
2018-11-14 14:02:09 +0900

    hw8 template copy

---
+++ hw8/HeapQueue.java
@@ -0,0 +1,24 @@
+ 
+
+public class HeapQueue<E extends Comparable<E>> implements Queue<E> {
+    private Heap<E> heap;
+    
+    public HeapQueue() {
+        heap = new Heap<E>();
+    }
+    public int size() {
+        //TODO: implement this method
+    }
+    public boolean isEmpty() {
+        //TODO: implement this method
+    }
+    public void enqueue(E e) {
+        //TODO: implement this method
+    }
+    public E dequeue() {
+        //TODO: implement this method
+    }
+    public E first() {
+        //TODO: implement this method
+    }
+}
---
+++ hw8/RBTreeQueue.java
@@ -0,0 +1,24 @@
+ 
+
+public class RBTreeQueue<E extends Comparable<E>> implements Queue<E> {
+    private RBTree<E> rbTree;
+    
+    public RBTreeQueue() {
+        rbTree = new RBTree<E>();
+    }
+    public int size() {
+        //TODO: implement this method
+    }
+    public boolean isEmpty() {
+        //TODO: implement this method
+    }
+    public void enqueue(E e) {
+        //TODO: implement this method
+    }
+    public E dequeue() {
+        //TODO: implement this method
+    }
+    public E first() {
+        //TODO: implement this method
+    }
+}
